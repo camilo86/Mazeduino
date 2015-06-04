3984e387ec6e62225595dfcee953e779e5efc08f
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2015-06-04 09:14:13 -0400

    created drivetrain as well as some simple driving methods
    Driving based on time as well as booleans work well

---
+++ Drivetrain.ino
@@ -0,0 +1,30 @@
+void driveForwardTime(double timeSeconds) {
+  leftDrive.run(FORWARD);
+  leftDrive.run(FORWARD);
+  leftDrive.setSpeed(255);
+  leftDrive.setSpeed(255);
+  delay(timeSeconds * 1000.0);
+  leftDrive.setSpeed(0);
+  rightDrive.setSpeed(0);
+}
+void driveBackwardTime(double timeSeconds) {
+  leftDrive.run(BACKWARD);
+  leftDrive.run(BACKWARD);
+  leftDrive.setSpeed(255);
+  leftDrive.setSpeed(255);
+  delay(timeSeconds * 1000.0);
+  leftDrive.setSpeed(0);
+  rightDrive.setSpeed(0);
+}
+
+void driveForward() {
+  leftDrive.run(FORWARD);
+  rightDrive.run(FORWARD);
+  leftDrive.setSpeed(255);
+  rightDrive.setSpeed(255);
+}
+
+void driveStop() {
+  leftDrive.run(RELEASE);
+  rightDrive.run(RELEASE);
+}
--- Mazeduino.ino
+++ Mazeduino.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <AFMotor.h>
 
+AF_DCMotor leftDrive(1);
+AF_DCMotor rightDrive(3);
+
+void setup() {
+  Serial.begin(9600);
+  leftDrive.run(RELEASE);
+  rightDrive.run(RELEASE);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  driveForwardTime(5);
+  driveStop();
+  delay(500000);
 }
